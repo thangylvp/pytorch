3f7bd2f2ffd10299fcc62f76fb89ac10e1314013
thangylvp <thang.yenlacvinhphuc.98@gmail.com>
2018-11-12 16:39:18 +0700

    Sample Dataloader

--- LearnPytorch/dataloading.py
+++ LearnPytorch/dataloading.py
@@ -131,6 +131,23 @@ def show_landmarks(image, landmarks):
     plt.pause(0.001)  # pause a bit so that plots are updated
 
 # plt.ion()   # interactive mode
+# Helper function to show a batch
+def show_landmarks_batch(sample_batched):
+    """Show image with landmarks for a batch of samples."""
+    images_batch, landmarks_batch = \
+            sample_batched['image'], sample_batched['landmarks']
+    batch_size = len(images_batch)
+    im_size = images_batch.size(2)
+
+    grid = utils.make_grid(images_batch)
+    plt.imshow(grid.numpy().transpose((1, 2, 0)))
+
+    for i in range(batch_size):
+        plt.scatter(landmarks_batch[i, :, 0].numpy() + i * im_size,
+                    landmarks_batch[i, :, 1].numpy(),
+                    s=10, marker='.', c='r')
+
+        plt.title('Batch from dataloader')
 
 
 landmarks_frame = pd.read_csv('data/faces/face_landmarks.csv')
@@ -169,7 +186,6 @@ for i in range(len(face_dataset)):
         plt.show()
         break
     
-'''
 scale = Rescale(256)
 crop = RandomCrop(128)
 composed = transforms.Compose([Rescale(256),
@@ -190,3 +206,35 @@ for i, tsfrm in enumerate([scale, crop, composed]):
     show_landmarks(**transformed_sample)
 
 plt.show()
+'''
+transformed_dataset = FaceLandmarksDataset(csv_file='data/faces/face_landmarks.csv',
+                                           root_dir='data/faces/',
+                                           transform=transforms.Compose([
+                                               Rescale(256),
+                                               RandomCrop(224),
+                                               ToTensor()
+                                           ]))
+
+for i in range(len(transformed_dataset)):
+    sample = transformed_dataset[i]
+
+    print(i, sample['image'].size(), sample['landmarks'].size())
+
+    if i == 3:
+        break
+
+dataloader = DataLoader(transformed_dataset, batch_size=4,
+                        shuffle=True, num_workers=4)
+
+for i_batch, sample_batched in enumerate(dataloader):
+    print(i_batch, sample_batched['image'].size(),
+          sample_batched['landmarks'].size())
+
+    # observe 4th batch and stop.
+    if i_batch == 3:
+        plt.figure()
+        show_landmarks_batch(sample_batched)
+        plt.axis('off')
+        plt.ioff()
+        plt.show()
+        break
