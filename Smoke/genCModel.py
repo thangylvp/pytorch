986c88bff5a525b9909acc74dc90df665e001322
thangylvp <thang.yenlacvinhphuc.98@gmail.com>
2019-03-07 11:44:20 +0700

    gen model for libtorch

---
+++ Smoke/genCModel.py
@@ -0,0 +1,29 @@
+from __future__ import print_function, division
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+
+# An instance of your model.
+model_ft = torchvision.models.resnet18()
+num_ftrs = model_ft.fc.in_features
+model_ft.fc = nn.Linear(num_ftrs, 2)
+
+
+model_ft.load_state_dict(torch.load('data/model/resnet18_1.pt'))
+model_ft.eval()
+
+# An example input you would normally provide to your model's forward() method.
+example = torch.rand(1, 3, 224, 224)
+
+# Use torch.jit.trace to generate a torch.jit.ScriptModule via tracing.
+traced_script_module = torch.jit.trace(model_ft, example)
+traced_script_module.save("data/model/model.pt")
