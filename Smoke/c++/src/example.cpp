c1e2c93329b4c089099086fb7fd1ff355841dceb
thangylvp <thang.yenlacvinhphuc.98@gmail.com>
2019-03-11 15:34:16 +0700

    Add cuda

--- Smoke/c++/src/example.cpp
+++ Smoke/c++/src/example.cpp
@@ -2,9 +2,14 @@
 #include <torch/script.h>
 #include <iostream>
 
+#define CUDA 
+
 int main() {
+	/* test tensor
 	torch::Tensor tensor = torch::rand({2, 3});
 	std::cout << tensor << std::endl;
+	*/
+
 
 	std::string model_path = "../../data/model/model.pt";
 
@@ -14,7 +19,23 @@ int main() {
 	if (module == nullptr) {
         std::cerr << "model load error " << std::endl;
     }
+
+#ifdef CUDA
+	module->to(at::kCUDA);
+#endif
+
     std::cout << "Model load ok.\n";
   	assert(module != nullptr);
   	std::cout << "ok\n";
+
+	std::vector<torch::jit::IValue> inputs;
+#ifdef CUDA
+	inputs.push_back(torch::ones({1, 3, 224, 224}).to(at::kCUDA));
+#else
+	inputs.push_back(torch::ones({1, 3, 224, 224}));
+#endif
+	// Execute the model and turn its output into a tensor.
+	at::Tensor output = module->forward(inputs).toTensor();
+
+	std::cout << output.slice(/*dim=*/1, /*start=*/0, /*end=*/5) << '\n';
 }
